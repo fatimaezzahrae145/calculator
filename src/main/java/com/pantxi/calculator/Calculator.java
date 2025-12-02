dffd7364f93b2673c3ed797f68e39bc77a7f6a2a
fatima ezzahrae <fatimaezzahraetaba@gmail.com>
2025-12-02 22:47:19 +0100

    ajout des méthodes add et divide + tests unitaires

--- src/main/java/com/pantxi/calculator/Calculator.java
+++ src/main/java/com/pantxi/calculator/Calculator.java
@@ -2,9 +2,14 @@ package com.pantxi.calculator;
 
     public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int opG, int opD) {
+        return  opG + opD;
     }
-}
+
+
+        public int divide(int opG, int opD) {
+            return opG / opD;
+        }
+    }
 
 
--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -7,7 +7,13 @@ public class CalculatorTest {
         Calculator calcul = new Calculator();
         int result = calcul.add(2, 3);
         assertEquals(5, result, "2 + 3 doit être égal à 5");
-
     }
+    @Test
+    public void Division() {
+        Calculator calcul = new Calculator();
+        int result = calcul.divide(8, 2);
+        assertEquals(4, result, "8/2 doit être égal à 4");
+    }
+
 
 }
